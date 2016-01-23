b90e44561018b4cfcf549184a27658205fd38fbb
Hoy <junghoyo92@gmail.com>
2016-01-23 14:45:54 -0800

    initial

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+// Defining a Global Variable listArray of strings
+// () initializes empty array
+var listArray = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet var toDoList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Syncing listArray with NSUserDefaults for permanent data storage if the NSUserDefaults exists
+        if NSUserDefaults.standardUserDefaults().objectForKey("listArray") != nil {
+            
+            listArray = NSUserDefaults.standardUserDefaults().objectForKey("listArray") as! [String]
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return listArray.count
+        
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = listArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            listArray.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(listArray, forKey: "listArray")
+            
+            toDoList.reloadData()
 
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoList.reloadData()
+    }
+    
 }
-
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    // Initializes the userInput text field
+    @IBOutlet var userInput: UITextField!
+    
+    // Initializes the addItem button
+    @IBAction func addItem(sender: AnyObject) {
+        
+        // Adds the userInput to the Global array for the to-do list when the button is pressed
+        listArray.append(userInput.text!)
+        
+        // Resets the userInput text field to nothing so one can enter multiple tasks
+        userInput.text = ""
+        
+        // Update NSUserDefaults for the permanent storage
+        NSUserDefaults.standardUserDefaults().setObject(listArray, forKey: "listArray")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Setting the userInput so that the textfield is delegated to the return functions
+        self.userInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,20 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Closes the keyboard by pressing anywhere
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    // Closes the keyboard by pressing return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
 
 }
 
